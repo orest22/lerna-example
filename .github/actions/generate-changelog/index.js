d2660218f8fd7862295e1fbf5b3c00db1667c33b
Orest Hazda <1497502+orest22@users.noreply.github.com>
2020-12-28 17:52:40 -0500

    ci: update release flow (#6)

--- .github/actions/generate-changelog/index.js
+++ .github/actions/generate-changelog/index.js
@@ -2,17 +2,16 @@ const cp = require('child_process');
 cp.execSync(`cd ${__dirname}; npm ci`);
 
 const core = require('@actions/core');
-// const github = require('@actions/github');
 const { Changelog } = require('lerna-changelog');
 const { load } = require('lerna-changelog/lib/configuration');
 
-// const tagTo = github.context.payload.pull_request.head.sha;
-// const tagFrom = github.context.payload.pull_request.base.sha;
+const tagFrom = core.getInput('from', { required: true });
+const tagTo = core.getInput('to', { required: true });
 
 const config = load();
 
 const cl = new Changelog(config);
 
-cl.createMarkdown()
+cl.createMarkdown({ tagFrom, tagTo })
   .then((changelog) => core.setOutput('changelog', JSON.stringify(changelog)))
   .catch((err) => core.warning(`Failed generating changelog ${err}`));
