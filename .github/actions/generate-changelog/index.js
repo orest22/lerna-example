c9ef98f4160b6315b6d1d56e99191b15098a8351
Orest Hazda <oresthazda@gmail.com>
2020-12-28 16:10:39 -0500

    add changelog custom action

---
+++ .github/actions/generate-changelog/index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+// const github = require('@actions/github');
+const { Changelog } = require('lerna-changelog');
+const { load } = require('lerna-changelog/lib/configuration');
+
+// const tagTo = github.context.payload.pull_request.head.sha;
+// const tagFrom = github.context.payload.pull_request.base.sha;
+
+const config = load();
+
+const cl = new Changelog(config);
+
+cl.createMarkdown({ tagFrom, tagTo })
+  .then((changelog) => core.setOutput('changelog', changelog))
+  .catch((err) => core.warning(`Failed generating changelog ${err}`));
