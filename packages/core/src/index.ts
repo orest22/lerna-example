dfce99e507a23ab94c2d2b0b7050544cca5c1b2a
Orest Hazda <oresthazda@gmail.com>
2020-12-28 18:00:12 -0500

    docs: update documentation

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,10 @@
 import utils from '@orest22/utils';
 
+/**
+ * Return sub of two numbers
+ * @param a
+ * @param b
+ */
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('boop');
