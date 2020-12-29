d5d25f67ab4825d083e779b125ee55da2e274379
Orest Hazda <oresthazda@gmail.com>
2020-12-29 11:27:52 -0500

    ci: update changelog action

--- .github/actions/generate-changelog/index.js
+++ .github/actions/generate-changelog/index.js
@@ -7,11 +7,17 @@ const { load } = require('lerna-changelog/lib/configuration');
 
 const tagFrom = core.getInput('from', { required: true });
 const tagTo = core.getInput('to', { required: true });
+const storybookUrl = 'https://www.npmjs.com/settings/orest22/packages';
 
 const config = load();
 
 const cl = new Changelog(config);
 
 cl.createMarkdown({ tagFrom, tagTo })
-  .then((changelog) => core.setOutput('changelog', JSON.stringify(changelog)))
+  .then((changelog) =>
+    core.setOutput(
+      'changelog',
+      JSON.stringify(`${changelog}\n\n**Storybook:** ${storybookUrl}`)
+    )
+  )
   .catch((err) => core.warning(`Failed generating changelog ${err}`));
