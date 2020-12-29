0ebb8890dfe0a9d208565e9c4934efa9b4ebbe9f
Orest Hazda <1497502+orest22@users.noreply.github.com>
2020-12-28 22:40:42 -0500

    ci: use rollup to replace version via tag name (#10)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,6 +4,7 @@ import utils from '@orest22/utils';
  * Return sub of two numbers
  * @param a
  * @param b
+ * @version __version__
  */
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
