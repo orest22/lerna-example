a037065431cf9ba583f81550119d22a46e8f4fa8
Orest Hazda <1497502+orest22@users.noreply.github.com>
2021-09-08 11:10:13 -0400

    fix: fix bug 1 (#19)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -8,7 +8,7 @@ import utils from '@orest22/utils';
  */
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+    console.log('fix bug');
   }
   return utils.sum(a, b);
 };
